1ae4663fc88180693596e070cdd1426ddf1769ec
Karpat221 <126197438+Karpt221@users.noreply.github.com>
2024-12-05 22:14:59 +0200

    Refactoring commit

--- scripts/map.js
+++ scripts/map.js
@@ -4,7 +4,7 @@ var map = L.map('map', {
     worldCopyJump: true // Ensures smooth interaction with wrapped maps  
 }).setView([50.32876033586117, 30.483001336281887], 6);
 
-map.options.maxZoom = 6; // Set maximum zoom level  
+map.options.maxZoom = 12; // Set maximum zoom level  
 
 // Add base map without labels  
 L.tileLayer('https://{s}.basemaps.cartocdn.com/light_nolabels/{z}/{x}/{y}{r}.png', {
@@ -20,15 +20,35 @@ L.tileLayer('https://{s}.basemaps.cartocdn.com/light_only_labels/{z}/{x}/{y}{r}.
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors &copy; <a href="https://carto.com/attributions">CARTO</a>'
 }).addTo(map);
 
+// Generate a random color  
+function getRandomColor() {
+    const letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Object to store assigned colors for each country  
+const countryColors = {};
+
 // Styling function for GeoJSON layer  
 function style(feature) {
+    const countryName = feature.properties.NAME;
+
+    // Assign a color to the country if it doesn't already have one  
+    if (!countryColors[countryName]) {
+        countryColors[countryName] = getRandomColor();
+    }
+
     return {
-        fillColor: 'transparent', // Transparent fill  
+        fillColor: countryColors[countryName], // Use the assigned color  
         weight: 1.5, // Border thickness  
         opacity: 1, // Border opacity  
         color: '#000000', // Black border color  
         dashArray: '', // Solid border  
-        fillOpacity: 0, // No fill  
+        fillOpacity: 0.2, // Semi-transparent fill  
         className: 'country-border' // Optional class for additional styling  
     };
 }
@@ -44,10 +64,6 @@ function highlightFeature(e) {
     });
 
     layer.bringToFront();
-    layer.bindPopup(layer.feature.properties.name, {
-        className: 'custom-popup',
-        autoPan: false // Prevent auto-panning when popup opens  
-    }).openPopup();
 }
 
 // Reset highlight on mouseout  
@@ -59,7 +75,7 @@ function resetHighlight(e) {
 // Zoom to feature on click  
 function zoomToFeature(e) {
     map.fitBounds(e.target.getBounds());
-    const countryName = e.target.feature.properties.name;
+    const countryName = e.target.feature.properties.NAME;
     if (countryHistories[countryName]) {
         openModal(countryName, countryHistories[countryName]);
     } else {
@@ -85,22 +101,145 @@ const customNames = {
 // Global variable for GeoJSON layer  
 var geojsonLayer;
 
-// Load and add GeoJSON data  
-fetch('https://raw.githubusercontent.com/johan/world.geo.json/master/countries.geo.json')
-    .then(response => response.json())
-    .then(data => {
-        // Modify country names before creating the layer  
-        data.features = data.features.map(feature => {
-            if (customNames[feature.properties.name]) {
-                feature.properties.name = customNames[feature.properties.name];
-            }
-            return feature;
-        });
+function fetchBoarder(boardersURL) {
+    fetch(boardersURL)
+        .then(response => response.json())
+        .then(data => {
+            data.features = data.features.map(feature => {
+                if (customNames[feature.properties.name]) {
+                    feature.properties.name = customNames[feature.properties.name];
+                }
+                return feature;
+            });
 
-        // Add GeoJSON layer with custom styling and event handlers  
-        geojsonLayer = L.geoJSON(data, {
-            style: style,
-            onEachFeature: onEachFeature
-        }).addTo(map);
-    })
-    .catch(error => console.error('Error loading GeoJSON:', error));
+            // Remove the previous GeoJSON layer if it exists  
+            if (geojsonLayer) {
+                map.removeLayer(geojsonLayer);
+            }
+
+            // Add GeoJSON layer with custom styling and event handlers  
+            geojsonLayer = L.geoJSON(data, {
+                style: style,
+                onEachFeature: onEachFeature
+            }).addTo(map);
+        })
+        .catch(error => console.error('Error loading historical GeoJSON:', error));
+}
+
+let borderURLs =
+{
+    "2010": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_2010.geojson',
+    "2000": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_2000.geojson',
+    "1994": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1994.geojson',
+    "1960": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1960.geojson',
+    "1945": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1945.geojson',
+    "1938": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1938.geojson',
+    "1920": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1920.geojson',
+    "1914": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1914.geojson',
+    "1900": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1900.geojson',
+    "1880": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1880.geojson',
+    "1815": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1815.geojson',
+    "1800": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1800.geojson',
+    "1783": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1783.geojson',
+    "1715": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1715.geojson',
+    "1700": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1700.geojson',
+    "1650": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1650.geojson',
+    "1600": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1600.geojson',
+    "1530": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1530.geojson',
+    "1500": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1500.geojson',
+    "1492": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1492.geojson',
+    "1400": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1400.geojson',
+    "1300": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1300.geojson',
+    "1279": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1279.geojson',
+    "1200": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1200.geojson',
+    "1100": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1100.geojson',
+    "1000": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_1000.geojson',
+    "900": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_900.geojson',
+    "800": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_800.geojson',
+    "700": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_700.geojson',
+    "600": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_600.geojson',
+    "500": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_500.geojson',
+    "400": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_400.geojson',
+    "300": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_300.geojson',
+    "200": 'https://raw.githubusercontent.com/aourednik/historical-basemaps/master/geojson/world_200.geojson',
+};
+
+fetchBoarder(borderURLs["2010"]);
+
+// Define a custom icon for the marker  
+var battleIcon = L.icon({
+    iconUrl: '../battle-svgrepo-com.svg', // Path to your custom icon image  
+    iconSize: [32, 32], // Size of the icon [width, height]  
+    iconAnchor: [0, 0], // Anchor point of the icon (center bottom)  
+    popupAnchor: [0, -32] // Anchor point for popups (optional)  
+});
+
+var diplomacyIcon = L.icon({
+    iconUrl: '../parthenon-landmark-svgrepo-com.svg', // Path to your custom icon image  
+    iconSize: [32, 32], // Size of the icon [width, height]  
+    iconAnchor: [0, 0], // Anchor point of the icon (center bottom)  
+    popupAnchor: [0, -32] // Anchor point for popups (optional)  
+});
+
+// // Add a marker for Ukraine  
+// var ukraineMarker = L.marker([50.32876033586117, 30.483001336281887], { icon: battleIcon }) // Coordinates for Kyiv, Ukraine  
+//     .addTo(map)
+//     .bindTooltip("6 травня: польські війська здобули Київ.", { permanent: true, direction: "top" }) // Add a label  
+//     .on('click', function () {
+//         openModal("", "<p style='font-size:25px;'>6 травня: польські війська здобули Київ.</p>");
+//     });
+
+
+var ukraineMarker = L.marker([50.32876033586117, 30.483001336281887], { icon: battleIcon }) // Coordinates for Kyiv, Ukraine  
+    .addTo(map)
+    .bindTooltip("6 травня 1920: польські війська здобули Київ.", {
+        permanent: true,
+        direction: "top",
+        className: "custom-tooltip" // Add a custom class  
+    }) // Add a label  
+    .on('click', function () {
+        openModal("",
+            `<h2>Київська операція Війська Польського (1920)</h2>
+            <p>
+            Ки́ївська опера́ція Ві́йська По́льського (1920) (Ки́ївська експеди́ція (пол. Wyprawa kijowska), також рідко Украї́нська опера́ція[1]) — наступальна операція Війська Польського і Армії УНР в квітні 1920 року, метою якої було визволити Київ від більшовиків.
+            </p>
+            <a href='https://uk.wikipedia.org/wiki/%D0%9A%D0%B8%D1%97%D0%B2%D1%81%D1%8C%D0%BA%D0%B0_%D0%BE%D0%BF%D0%B5%D1%80%D0%B0%D1%86%D1%96%D1%8F_%D0%92%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%B0_%D0%9F%D0%BE%D0%BB%D1%8C%D1%81%D1%8C%D0%BA%D0%BE%D0%B3%D0%BE_(1920)'>Wikipedia</a>
+            `);
+    });
+
+
+var ukraineMarker = L.marker([50.08804, 14.42076], { icon: diplomacyIcon }) // Coordinates for Kyiv, Ukraine  
+    .addTo(map)
+    .bindTooltip("липень 1920: створення Української військової організації (УВО)", {
+        permanent: true,
+        direction: "top",
+        className: "custom-tooltip" // Add a custom class  
+    }) // Add a label  
+    .on('click', function () {
+        openModal("",
+            `<h2>Українська військова організація</h2>
+            <p>
+            Украї́нська військо́ва організа́ція (УВО) — військова революційно-політична формація, що постала 1920 року заходами старшин різних українських армій: Січових Стрільців (ідеологічно-політичний вклад), Української Галицької армії, в основному її VI Равської бригади (бойовий та організаційний елемент).
+            </p>
+            <p>9 квітня 2015 року Верховна Рада України ухвалила закон «Про правовий статус та вшанування пам'яті борців за незалежність України у ХХ столітті» авторства Юрія Шухевича, в якому вказано перелік організацій, які визнаються таким, що боролись за незалежність України, серед них є й Українська військова організація.[1]</p>
+            <a href='https://uk.wikipedia.org/wiki/%D0%A3%D0%BA%D1%80%D0%B0%D1%97%D0%BD%D1%81%D1%8C%D0%BA%D0%B0_%D0%B2%D1%96%D0%B9%D1%81%D1%8C%D0%BA%D0%BE%D0%B2%D0%B0_%D0%BE%D1%80%D0%B3%D0%B0%D0%BD%D1%96%D0%B7%D0%B0%D1%86%D1%96%D1%8F'>Wikipedia</a>
+            `);
+    });  
+
+
+    var ukraineMarker = L.marker([52.217, 21.033], { icon: diplomacyIcon }) // Coordinates for Kyiv, Ukraine  
+    .addTo(map)
+    .bindTooltip("25 квітня 1920: підписання Варшавського договору", {
+        permanent: true,
+        direction: "top",
+        className: "custom-tooltip" // Add a custom class  
+    }) // Add a label  
+    .on('click', function () {
+        openModal("",
+            `<h2>Варшавський договір (1920)</h2>
+            <p>
+            Варша́вський договір, або Договір Пілсудський — Петлюра — політичні й військові конвенції, підписані представниками Польщі і Української Народної Республіки, які розроблялись таємно від уряду УНР й польського сейму. За ними в обмін на визнання незалежності УНР і військову допомогу Симон Петлюра погоджувався визнати українсько-польський кордон по річці Збруч і далі по Прип'яті до її гирла. Згідно з договором польський уряд Юзефа Пілсудського відмовився від намірів розширити територію Польщі до кордонів Речі Посполитої 1772 року та визнав УНР. До Польщі мала відійти Східна Галичина та 5 повітів Волині.
+            </p>
+            <a href='https://uk.wikipedia.org/wiki/%D0%92%D0%B0%D1%80%D1%88%D0%B0%D0%B2%D1%81%D1%8C%D0%BA%D0%B8%D0%B9_%D0%B4%D0%BE%D0%B3%D0%BE%D0%B2%D1%96%D1%80_(1920)'>Wikipedia</a>
+            `);
+    });  
--- scripts/slider.js
+++ scripts/slider.js
@@ -22,12 +22,6 @@ function getCurrentYear() {
     return currentYear;
 }
 
-// Function to handle year change  
-function onYearChange(year) {
-    // Можна додати додаткову логіку обробки зміни року  
-    console.log('Year changed to:', year);
-    return year;
-}
 
 // Function to update the slider background gradient  
 function updateSliderBackground(value) {
@@ -53,18 +47,55 @@ function updateSliderBackground(value) {
 
 // Function to update the year display and slider position  
 function updateYear(year) {
-    if (year >= 1100 && year <= 2024) {
+    // Convert the year to display format (e.g., "2000 BC" for negative years)  
+    const yearText = year >= 0 ? year.toString() : `${Math.abs(year)} BC`;  
+    yearDisplay.textContent = yearText;  
+
+    // Hide error message and update slider background  
+    if (year >= 200 && year <= 2024) {  
         errorMessage.style.display = 'none'; // Hide error message  
-        yearDisplay.textContent = year >= 0 ? year : `${Math.abs(year)} BC`;
-        yearSlider.value = year;
         currentYear = year; // Update the current year variable  
         updateSliderBackground(year); // Update the gradient  
-        onYearChange(year); // Trigger year change handler  
-    } else {
+    } else {  
         errorMessage.style.display = 'block'; // Show error message  
-    }
-}
+        return; // Exit the function if the year is out of bounds  
+    }  
 
+    // Convert borderURLs keys to an array of numbers (handle BC years)  
+    const borderYears = Object.keys(borderURLs).map((key) => {  
+        return key.includes('BC') ? -parseInt(key.replace('BC', '').trim()) : parseInt(key);  
+    });  
+
+    // Sort the years in ascending order  
+    borderYears.sort((a, b) => a - b);  
+
+    // Find the closest border year (favoring the later one in case of ties)  
+    let closestYear = null;  
+    for (let i = 0; i < borderYears.length; i++) {  
+        if (year < borderYears[i]) {  
+            // If the year is less than the current border year, choose the previous one  
+            closestYear = i > 0 ? borderYears[i - 1] : borderYears[i];  
+            break;  
+        }  
+    }  
+
+    // If no year is less than the current year, choose the last border year  
+    if (closestYear === null) {  
+        closestYear = borderYears[borderYears.length - 1];  
+    }  
+
+    // Convert the closest year back to the string format used in borderURLs  
+    const closestYearText = closestYear >= 0 ? closestYear.toString() : `${Math.abs(closestYear)} BC`;  
+
+    // Fetch the border data for the closest year  
+    if (borderURLs.hasOwnProperty(closestYearText)) {  
+        if (geojsonLayer) {  
+            map.removeLayer(geojsonLayer); // Remove the previous GeoJSON layer  
+        }  
+        fetchBoarder(borderURLs[closestYearText]); // Fetch borders for the closest year  
+    }  
+}
+let lastVal = parseInt(yearSlider.value);
 // Event Listener: Slider Input  
 yearSlider.addEventListener('input', () => {
     const year = parseInt(yearSlider.value);
@@ -96,6 +127,134 @@ yearDisplay.addEventListener('keypress', (e) => {
 updateSliderBackground(yearSlider.value);
 
 
+// // DOM Elements  
+// const yearSlider = document.getElementById('year-slider');  
+// const dateInput = document.getElementById('date-input');  
+// const errorMessage = document.getElementById('error-message');  
 
+// // Colors array for gradient  
+// const colors = [  
+//     '#3E1F09', // 1100  
+//     '#614332', // 1200  
+//     '#856758', // 1300  
+//     '#A88B7E', // 1400  
+//     '#CCAFA4', // 1500  
+//     '#E0C7BF', // 1600  
+//     '#EFE0DB', // 1700  
+// ];  
 
-const year = getCurrentYear();
+// // Function to update the slider background gradient  
+// function updateSliderBackground(value) {  
+//     const min = parseInt(yearSlider.min);  
+//     const max = parseInt(yearSlider.max);  
+//     const percentage = ((value - min) / (max - min)) * 100;  
+
+//     let gradientStops = '';  
+
+//     // Add color stops up to the current position  
+//     for (let i = 0; i < colors.length; i++) {  
+//         const stopPercentage = (i / (colors.length - 1)) * percentage;  
+//         if (stopPercentage <= percentage) {  
+//             gradientStops += `${colors[i]} ${stopPercentage}%, `;  
+//         }  
+//     }  
+
+//     // Add white color from the current position to the end  
+//     gradientStops += `#FFFFFF ${percentage}%, #FFFFFF 100%`;  
+
+//     yearSlider.style.background = `linear-gradient(to right, ${gradientStops})`;  
+// }  
+
+// // Function to update the date input and slider position  
+// function updateDateAndSlider(dayOffset) {  
+//     const currentDate = new Date(dateInput.value);  
+
+//     // Adjust the date by the given day offset  
+//     currentDate.setDate(currentDate.getDate() + dayOffset);  
+
+//     // Update the date input value  
+//     dateInput.value = currentDate.toISOString().split('T')[0];  
+
+//     // Update the slider value based on the year in the date input  
+//     const year = currentDate.getFullYear();  
+//     if (year >= 200 && year <= 2024) {  
+//         yearSlider.value = year;  
+//         updateSliderBackground(year);  
+//         errorMessage.style.display = 'none'; // Hide error message  
+//     } else {  
+//         errorMessage.style.display = 'block'; // Show error message if year is out of bounds  
+//     }  
+
+//     // Fetch country boundaries for the updated year  
+//     fetchBoundariesForYear(year);  
+// }  
+
+// // Function to fetch boundaries for the given year  
+// function fetchBoundariesForYear(year) {  
+//     // Convert borderURLs keys to an array of numbers (handle BC years)  
+//     const borderYears = Object.keys(borderURLs).map((key) => {  
+//         return key.includes('BC') ? -parseInt(key.replace('BC', '').trim()) : parseInt(key);  
+//     });  
+
+//     // Sort the years in ascending order  
+//     borderYears.sort((a, b) => a - b);  
+
+//     // Find the closest border year (favoring the later one in case of ties)  
+//     let closestYear = null;  
+//     for (let i = 0; i < borderYears.length; i++) {  
+//         if (year < borderYears[i]) {  
+//             // If the year is less than the current border year, choose the previous one  
+//             closestYear = i > 0 ? borderYears[i - 1] : borderYears[i];  
+//             break;  
+//         }  
+//     }  
+
+//     // If no year is less than the current year, choose the last border year  
+//     if (closestYear === null) {  
+//         closestYear = borderYears[borderYears.length - 1];  
+//     }  
+
+//     // Convert the closest year back to the string format used in borderURLs  
+//     const closestYearText = closestYear >= 0 ? closestYear.toString() : `${Math.abs(closestYear)} BC`;  
+
+//     // Fetch the border data for the closest year  
+//     if (borderURLs.hasOwnProperty(closestYearText)) {  
+//         if (geojsonLayer) {  
+//             map.removeLayer(geojsonLayer); // Remove the previous GeoJSON layer  
+//         }  
+//         fetchBoarder(borderURLs[closestYearText]); // Fetch borders for the closest year  
+//     }  
+// }  
+
+// // Event Listener: Slider Input  
+// yearSlider.addEventListener('input', () => {  
+//     const sliderYear = parseInt(yearSlider.value);  
+
+//     // Update the date input to match the slider's year  
+//     const currentDate = new Date(dateInput.value);  
+//     currentDate.setFullYear(sliderYear);  
+//     dateInput.value = currentDate.toISOString().split('T')[0];  
+
+//     // Update the slider background and fetch boundaries  
+//     updateSliderBackground(sliderYear);  
+//     fetchBoundariesForYear(sliderYear);  
+// });  
+
+// // Event Listener: Date Input Change  
+// dateInput.addEventListener('change', () => {  
+//     const selectedDate = new Date(dateInput.value);  
+//     const year = selectedDate.getFullYear();  
+
+//     // Update the slider to match the date input's year  
+//     if (year >= 200 && year <= 2024) {  
+//         yearSlider.value = year;  
+//         updateSliderBackground(year);  
+//         errorMessage.style.display = 'none'; // Hide error message  
+//         fetchBoundariesForYear(year);  
+//     } else {  
+//         errorMessage.style.display = 'block'; // Show error message if year is out of bounds  
+//     }  
+// });  
+
+// // Initialize the slider background  
+// updateSliderBackground(yearSlider.value);
